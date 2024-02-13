99df1f4ed8f8f82d1850670680753b95c3b29325
Iain Emslie <iainemslie@gmail.com>
2024-02-13 11:20:28 -0500

    basic window

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # ClearCodeSuperMarioPyGame
 
 [![IMAGE ALT TEXT HERE](https://img.youtube.com/vi/WViyCAa6yLI/0.jpg)](https://www.youtube.com/watch?v=WViyCAa6yLI)
+
+## Environment Setup
+
+```sh
+$ python -m venv pirateenv
+$ source ./pirateenv/Scripts/activate
+$ pip install requirements.txt
+```
+
+## Pygame Note
+
+`pygame-ce` is used instead of `pygame` since it has
+
+- frequent updates
+- faster runtime
+- backwards-compatibility
--- code/main.py
+++ code/main.py
@@ -1,29 +1,23 @@
-from settings import * 
-from level import Level
-from pytmx.util_pygame import load_pygame
-from os.path import join
+from settings import *
+
 
 class Game:
-	def __init__(self):
-		pygame.init()
-		self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-		pygame.display.set_caption('Super Pirate World')
-		self.clock = pygame.time.Clock()
+    def __init__(self):
+        pygame.init()
+        self.display_surface = pygame.display.set_mode(
+            (WINDOW_WIDTH, WINDOW_HEIGHT))
+        pygame.display.set_caption('Super Pirate World')
 
-		self.tmx_maps = {0: load_pygame(join('..', 'data', 'levels', 'omni.tmx'))}
-		self.current_stage = Level(self.tmx_maps[0])
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
 
-	def run(self):
-		while True:
-			dt = self.clock.tick() / 1000
-			for event in pygame.event.get():
-				if event.type == pygame.QUIT:
-					pygame.quit()
-					sys.exit()
+            pygame.display.update()
 
-			self.current_stage.run(dt)
-			pygame.display.update()
 
-if __name__ == '__main__':
-	game = Game()
-	game.run()
+if __name__ == "__main__":
+    game = Game()
+    game.run()
