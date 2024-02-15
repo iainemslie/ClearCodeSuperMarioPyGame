a5621f76d63ed94ea57c6b2fc007e47cfbbcbd35
Iain Emslie <iainemslie@gmail.com>
2024-02-15 12:31:29 -0500

    add missing graphic details

--- code/main.py
+++ code/main.py
@@ -28,11 +28,16 @@ class Game:
     def import_assets(self):
         self.level_frames = {
             'flag': import_folder('graphics', 'level', 'flag'),
-            'saw': import_folder('graphics', 'level', 'saw'),
             'saw': import_folder('graphics', 'enemies', 'saw', 'animation'),
-            'floor_spike': import_folder('graphics', 'enemies', 'floor_spikes')
+            'floor_spike': import_folder('graphics', 'enemies', 'floor_spikes'),
+            'palms': import_sub_folders('graphics', 'level', 'palms'),
+            'candle': import_folder('graphics', 'level', 'candle'),
+            'window': import_folder('graphics', 'level', 'window'),
+            'big_chain': import_folder('graphics', 'level', 'big_chains'),
+            'small_chain': import_folder('graphics', 'level', 'small_chains'),
+            'candle_light': import_folder('graphics', 'level', 'candle light'),
         }
-        print(self.level_frames)
+        print(self.level_frames['palms'])
 
     def run(self):
         while True:
